e3508ab982b1577531810eda16252e8b27f32121
KaushalxRauniyar <kaushalrauniyar1l@gmail.com>
2025-04-06 00:30:22 +0530

    final commit

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-export const getPrediction = async (formData) => {
+export const getPrediction=async(formData)=>
+   {
   try {
-    const response = await axios.post('http://localhost:5000/predict', formData, {
-      headers: { 'Content-Type': 'application/json' }
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error getting prediction:', error);
+    const response=await axios.post('http://localhost:5000/predict',formData,
+       {
+      headers:{'Content-Type':'application/json'}
+  });
+    return response.data
+  } catch(error){
+    console.error('Error getting prediction:',error);
     throw error;
   }
 };
