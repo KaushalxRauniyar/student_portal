629dfe2036d5061f0c930a7aa7d54e9e9d553ba9
kundanyad <yadavkundan@782.gamil.com>
2025-04-22 23:55:03 +0530

    commit

--- backendapk/config/multer.js
+++
@@ -1,6 +0,0 @@
-import multer from 'multer'
-
-const storage=multer.diskStorage({})
-const Upload=multer({storage})
-
-export default Upload
--- backendapk/route/Feedbackroute.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import addFeedback from "../controller/FeedbackController.js"
-const FeedRouter=express.Router()
-
-FeedRouter.post("/add",addFeedback)
-
-export default FeedRouter
